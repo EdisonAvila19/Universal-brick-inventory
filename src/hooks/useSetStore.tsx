859821f9fd83e6883581cd3e9011b5f34bb37167
edison <edison_20_04@hotmail.com>
2026-05-19 23:05:31 -0500

    refactor: Update state management in useSetStore for clarity and consistency

--- src/hooks/useSetStore.tsx
+++ src/hooks/useSetStore.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "preact/hooks";
+import { useStore } from '@nanostores/preact';
+
 import type { BrickRecord, SetRecord } from "@/types/archiveData";
-import { GetNewBricksForSet } from '@/utils/bricksData';
+import { GetNewBricksForSet } from '@utils/bricksData';
+import { $bricks } from '@stores/storage-bricks'
 
 export function useSetStore (activeSetNumber: string, initialSetBricks: BrickRecord[], sets: SetRecord[], setSelectedSet: (set: SetRecord | null) => void)  {
-  const [bricks, setBricks] = useState(initialSetBricks);
+  const fullBricks = useStore($bricks);
+  const [setBricks, setSetBricks] = useState(initialSetBricks);
   const [totalRequired, setTotalRequired] = useState(initialSetBricks.reduce((acc, brick) => acc + brick.required, 0));
   const [totalOwned, setTotalOwned] = useState(initialSetBricks.reduce((acc, brick) => acc + Math.min(brick.required, brick.stock), 0));
   
   const fetchBricks = async () => {
     const newBricks = await GetNewBricksForSet(activeSetNumber)
-    setBricks(newBricks);
+    setSetBricks(newBricks);
   }
 
   useEffect(() => {
@@ -20,9 +24,13 @@ export function useSetStore (activeSetNumber: string, initialSetBricks: BrickRec
   }, [sets])
 
   useEffect(() => {
-    setTotalRequired(bricks.reduce((acc, brick) => acc + brick.required, 0));
-    setTotalOwned(bricks.reduce((acc, brick) => acc + Math.min(brick.required, brick.stock), 0));
-  }, [bricks])
+    setTotalRequired(setBricks.reduce((acc, brick) => acc + brick.required, 0));
+    setTotalOwned(setBricks.reduce((acc, brick) => acc + Math.min(brick.required, brick.stock), 0));
+  }, [setBricks])
 
-  return { fetchBricks, totalRequired, totalOwned, bricks, setBricks };
+  useEffect(() => {
+    fetchBricks();
+  }, [fullBricks])
+
+  return { fetchBricks, totalRequired, totalOwned, bricks: setBricks, setBricks: setSetBricks };
 }
